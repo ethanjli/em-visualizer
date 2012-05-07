9fa7ecdd140fcc595e683efdbd7695b4ca1d5ed9
lietk12 <lietk12@gmail.com>
2012-05-07 14:38:52 -0400

    Protected arrowhead from disappearing when extremely high magnitudes are measured; minor improvements to implementation of graphics for arrow

--- js/em-entities/e-probes.js
+++ js/em-entities/e-probes.js
@@ -37,7 +37,8 @@ var EFieldVector = new Class({
 			strokeColor: "black",
 			strokeWidth: 2
 		};
-		arrowhead.rotate(-30);
+		arrowhead.rotate(-150);
+		// Draw the arrowtail for the vector
 		var arrowtail = new Path.Line(this.getCanvasCoordinates(), this.getCanvasCoordinates());
 		arrowtail.style = {
 			strokeColor: "black",
@@ -63,22 +64,23 @@ var EFieldVector = new Class({
 		var vectorData = this.measure(universe).to3D();
 		// Determine information about vector arrow
 		var vectorMagnitude = vectorData.modulus() * Math.pow(10, 9);
-		if (vectorMagnitude > 1) {
-			var vectorOrientation = Vector.i.cross(vectorData).toUnitVector().e(3); // Direction of -1 means the arrow is clockwise from i; direction of 1 means the arrow is counterclockwise from i
-			var vectorAngle = -1 * vectorOrientation * Vector.i.angleFrom(vectorData) * 180 / Math.PI;
-		} else {
+		if (vectorMagnitude > Math.pow(10, 20) || vectorMagnitude == 0) {
+			vectorMagnitude = 0;
 			vectorAngle = 0;
-		}
-		var currentMagnitude = this.getGraphics().vector.magnitude;
-		var currentAngle = this.getGraphics().vector.angle;
-		// Modify the arrowhead-end of the arrow
-		if (currentMagnitude > 1) {
-			// Scale the vector
-			var newVector = this.getGraphics().vector.arrowtail.lastSegment.point.subtract(this.getGraphics().vector.arrowtail.firstSegment.point).multiply(vectorMagnitude / currentMagnitude);
-			this.getGraphics().vector.arrowtail.lastSegment.point = newVector.add(this.getCanvasCoordinates());
 		} else {
+			var vectorOrientation = -1 * Vector.i.cross(vectorData).toUnitVector().e(3); // Direction of 1 means the arrow is clockwise from j; direction of -1 means the arrow is counterclockwise from j
+			var vectorAngle = vectorOrientation * vectorData.angleFrom(Vector.i) * 180 / Math.PI;
+		}
+		var previousMagnitude = this.getGraphics().vector.magnitude;
+		var previousAngle = this.getGraphics().vector.angle;
+		// Modify the arrowhead-end of the arrow
+		if (previousMagnitude == 0) { // The vector was previously drawn as zero
 			// Make a new endpoint
 			this.getGraphics().vector.arrowtail.lastSegment.point = (new Point(vectorMagnitude, 0)).add(this.getCanvasCoordinates());
+		} else {
+			// Scale the vector
+			var newVector = this.getGraphics().vector.arrowtail.lastSegment.point.subtract(this.getGraphics().vector.arrowtail.firstSegment.point).multiply(vectorMagnitude / previousMagnitude);
+			this.getGraphics().vector.arrowtail.lastSegment.point = newVector.add(this.getCanvasCoordinates());
 		}
 		this.getGraphics().vector.magnitude = vectorMagnitude;
 		// Move the arrowhead
@@ -86,8 +88,8 @@ var EFieldVector = new Class({
 		// Set the new probe-end of the arrow
 		this.getGraphics().vector.arrowtail.firstSegment.point = this.getCanvasCoordinates();
 		// Rotate the arrow accordingly
-		this.getGraphics().vector.arrowtail.rotate(vectorAngle - currentAngle, this.getGraphics().vector.arrowtail.firstSegment.point);
-		this.getGraphics().vector.arrowhead.rotate(vectorAngle - currentAngle, this.getGraphics().vector.arrowtail.firstSegment.point);
+		this.getGraphics().vector.arrowtail.rotate(vectorAngle - previousAngle, this.getGraphics().vector.arrowtail.firstSegment.point);
+		this.getGraphics().vector.arrowhead.rotate(vectorAngle - previousAngle, this.getGraphics().vector.arrowtail.firstSegment.point);
 		this.getGraphics().vector.angle = vectorAngle;
 		return true; // bool
 	},
@@ -95,7 +97,7 @@ var EFieldVector = new Class({
 
 // Gets the E Field direction vector for a point in the universe
 var EFieldDirection = new Class({
-	Extends: EField,
+	Extends: EFieldVector,
 	
 	initialize: function(properties, universe) { // Object
 		// Send up to parent
@@ -103,10 +105,6 @@ var EFieldDirection = new Class({
 		// Handle e-field-direction-probe-specific constants
 		this.getType().push("Direction Vector");
 		// Handle e-field-direction-probe-specific variables
-	},
-	initializeGraphics: function(universe) { // Universe
-		this.parent(universe);
-		this.refreshGraphics(universe);
 	},
 	
 	measure: function(universe) { // Universe
